1275f92cd1f26319b604731f8a72057c87aa7e9b
ohprettyhak <me@haklee.me>
2025-07-14 10:28:21 +0900

    feat(react): add `Accordion` component and update type definitions

---
+++ packages/react/lib/components/Accordion/Accordion.d.ts
@@ -0,0 +1,7 @@
+import * as AccordionPrimitive from '@radix-ui/react-accordion';
+export declare const Accordion: import("react").ForwardRefExoticComponent<(AccordionPrimitive.AccordionSingleProps | AccordionPrimitive.AccordionMultipleProps) & import("react").RefAttributes<HTMLDivElement>> & {
+    Item: import("react").ForwardRefExoticComponent<AccordionPrimitive.AccordionItemProps & import("react").RefAttributes<HTMLDivElement>>;
+    Header: import("react").ForwardRefExoticComponent<AccordionPrimitive.AccordionHeaderProps & import("react").RefAttributes<HTMLHeadingElement>>;
+    Trigger: import("react").ForwardRefExoticComponent<AccordionPrimitive.AccordionTriggerProps & import("react").RefAttributes<HTMLButtonElement>>;
+    Content: import("react").ForwardRefExoticComponent<AccordionPrimitive.AccordionContentProps & import("react").RefAttributes<HTMLDivElement>>;
+};
