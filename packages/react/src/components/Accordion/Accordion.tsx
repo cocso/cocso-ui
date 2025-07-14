64e2ebd47c8b6ef10be58ca7eb290721e05c1b99
ohprettyhak <me@haklee.me>
2025-07-14 10:27:59 +0900

    feat(react): add `Accordion` component and update package version

---
+++ packages/react/src/components/Accordion/Accordion.tsx
@@ -0,0 +1,8 @@
+import * as AccordionPrimitive from '@radix-ui/react-accordion';
+
+export const Accordion = Object.assign(AccordionPrimitive.Root, {
+  Item: AccordionPrimitive.Item,
+  Header: AccordionPrimitive.Header,
+  Trigger: AccordionPrimitive.Trigger,
+  Content: AccordionPrimitive.Content,
+});
