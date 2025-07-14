c8357db579f1ebc3b56f99471f968835a89c6b75
ohprettyhak <me@haklee.me>
2025-07-14 14:14:10 +0900

    feat(react): remove unused AccordionChevron component and update package version

--- packages/react/src/components/Accordion/Accordion.tsx
+++ packages/react/src/components/Accordion/Accordion.tsx
@@ -2,24 +2,6 @@ import * as AccordionPrimitive from '@radix-ui/react-accordion';
 import { ComponentPropsWithoutRef, ComponentRef, forwardRef } from 'react';
 import { createClassName } from '../../utils/cn';
 
-const AccordionChevron = () => (
-  <div className="cocso-accordion-chevron">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="m6 9 6 6 6-6" />
-    </svg>
-  </div>
-);
-
 const AccordionContent = forwardRef<
   ComponentRef<typeof AccordionPrimitive.Content>,
   ComponentPropsWithoutRef<typeof AccordionPrimitive.Content>
@@ -31,7 +13,7 @@ const AccordionContent = forwardRef<
 const AccordionTrigger = forwardRef<
   ComponentRef<typeof AccordionPrimitive.Trigger>,
   ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger>
->(({ className, children, ...props }, ref) => {
+>(({ className, ...props }, ref) => {
   const classNames = createClassName('cocso-accordion-trigger', {}, [], className);
   return <AccordionPrimitive.Trigger ref={ref} className={classNames} {...props} />;
 });
