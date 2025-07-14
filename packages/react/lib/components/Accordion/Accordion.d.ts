e10e8f50eaef7ba9ef90cdfc4763cb237fe468bd
ohprettyhak <me@haklee.me>
2025-07-14 14:52:46 +0900

    feat(react): enhance Accordion component with chevron support and update package versions

--- packages/react/lib/components/Accordion/Accordion.d.ts
+++ packages/react/lib/components/Accordion/Accordion.d.ts
@@ -2,6 +2,8 @@ import * as AccordionPrimitive from '@radix-ui/react-accordion';
 export declare const Accordion: import("react").ForwardRefExoticComponent<(AccordionPrimitive.AccordionSingleProps | AccordionPrimitive.AccordionMultipleProps) & import("react").RefAttributes<HTMLDivElement>> & {
     Item: import("react").ForwardRefExoticComponent<AccordionPrimitive.AccordionItemProps & import("react").RefAttributes<HTMLDivElement>>;
     Header: import("react").ForwardRefExoticComponent<AccordionPrimitive.AccordionHeaderProps & import("react").RefAttributes<HTMLHeadingElement>>;
-    Trigger: import("react").ForwardRefExoticComponent<Omit<AccordionPrimitive.AccordionTriggerProps & import("react").RefAttributes<HTMLButtonElement>, "ref"> & import("react").RefAttributes<HTMLButtonElement>>;
+    Trigger: import("react").ForwardRefExoticComponent<{
+        chevron?: boolean;
+    } & Omit<AccordionPrimitive.AccordionTriggerProps & import("react").RefAttributes<HTMLButtonElement>, "ref"> & import("react").RefAttributes<HTMLButtonElement>>;
     Content: import("react").ForwardRefExoticComponent<Omit<AccordionPrimitive.AccordionContentProps & import("react").RefAttributes<HTMLDivElement>, "ref"> & import("react").RefAttributes<HTMLDivElement>>;
 };
--- packages/react/src/components/Accordion/Accordion.tsx
+++ packages/react/src/components/Accordion/Accordion.tsx
@@ -1,6 +1,47 @@
 import * as AccordionPrimitive from '@radix-ui/react-accordion';
 import { ComponentPropsWithoutRef, ComponentRef, forwardRef } from 'react';
 import { createClassName } from '../../utils/cn';
+import { Slottable } from '@radix-ui/react-slot';
+
+const AccordionItem = forwardRef<
+  ComponentRef<typeof AccordionPrimitive.Item>,
+  ComponentPropsWithoutRef<typeof AccordionPrimitive.Item>
+>(({ className, ...props }, ref) => {
+  const classNames = createClassName('cocso-accordion-item', {}, [], className);
+  return <AccordionPrimitive.Item ref={ref} className={classNames} {...props} />;
+});
+
+type TriggerProps = {
+  chevron?: boolean;
+} & ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger>;
+
+const AccordionTrigger = forwardRef<ComponentRef<typeof AccordionPrimitive.Trigger>, TriggerProps>(
+  ({ className, children, chevron = true, ...props }, ref) => {
+    const classNames = createClassName('cocso-accordion-trigger', {}, [], className);
+    return (
+      <AccordionPrimitive.Trigger ref={ref} className={classNames} {...props}>
+        <Slottable>{children}</Slottable>
+        {chevron && (
+          <div className="cocso-accordion-chevron">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="m6 9 6 6 6-6" />
+            </svg>
+          </div>
+        )}
+      </AccordionPrimitive.Trigger>
+    );
+  },
+);
 
 const AccordionContent = forwardRef<
   ComponentRef<typeof AccordionPrimitive.Content>,
@@ -8,14 +49,6 @@ const AccordionContent = forwardRef<
 >(({ className, ...props }, ref) => {
   const classNames = createClassName('cocso-accordion-content', {}, [], className);
   return <AccordionPrimitive.Content ref={ref} className={classNames} {...props} />;
-});
-
-const AccordionTrigger = forwardRef<
-  ComponentRef<typeof AccordionPrimitive.Trigger>,
-  ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger>
->(({ className, ...props }, ref) => {
-  const classNames = createClassName('cocso-accordion-trigger', {}, [], className);
-  return <AccordionPrimitive.Trigger ref={ref} className={classNames} {...props} />;
 });
 
 export const Accordion = Object.assign(AccordionPrimitive.Root, {
