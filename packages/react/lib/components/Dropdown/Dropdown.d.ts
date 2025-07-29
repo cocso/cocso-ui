a7b5be4ac8670c955ba7f9424f1c334a36cdbbf2
Hak Lee <me@haklee.me>
2025-07-29 15:21:41 +0900

    feat(react): add `Popover` component (#11)
    * feat(react): add `Popover` component and update package versions
    
    * fix(deps): update dependencies in bun.lock and bump package versions

--- packages/react/lib/components/Dropdown/Dropdown.d.ts
+++ packages/react/lib/components/Dropdown/Dropdown.d.ts
@@ -2,6 +2,6 @@ import * as DropdownPrimitive from '@radix-ui/react-dropdown-menu';
 export declare const Dropdown: import("react").FC<DropdownPrimitive.DropdownMenuProps> & {
     Trigger: import("react").ForwardRefExoticComponent<DropdownPrimitive.DropdownMenuTriggerProps & import("react").RefAttributes<HTMLButtonElement>>;
     Portal: import("react").FC<DropdownPrimitive.DropdownMenuPortalProps>;
-    Content: import("react").ForwardRefExoticComponent<Omit<DropdownPrimitive.DropdownMenuContentProps & import("react").RefAttributes<HTMLDivElement>, "ref"> & import("react").RefAttributes<never>>;
-    Item: import("react").ForwardRefExoticComponent<Omit<DropdownPrimitive.DropdownMenuItemProps & import("react").RefAttributes<HTMLDivElement>, "ref"> & import("react").RefAttributes<never>>;
+    Content: import("react").ForwardRefExoticComponent<Omit<DropdownPrimitive.DropdownMenuContentProps & import("react").RefAttributes<HTMLDivElement>, "ref"> & import("react").RefAttributes<HTMLDivElement>>;
+    Item: import("react").ForwardRefExoticComponent<Omit<DropdownPrimitive.DropdownMenuItemProps & import("react").RefAttributes<HTMLDivElement>, "ref"> & import("react").RefAttributes<HTMLDivElement>>;
 };
