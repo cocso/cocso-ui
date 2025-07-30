5fda633a4ca61516a940a0bfb0aa5478d0671127
ohprettyhak <me@haklee.me>
2025-07-30 14:12:48 +0900

    feat(icons): add new React icon components (HMPLogo, MenuIcon, CloseIcon, DeleteIcon, OpenInNewIcon, ArrowIOSForwardIcon, SideNavigationIcon)

--- packages/react-icons/src/components/brand/index.ts
+++ packages/react-icons/src/components/brand/index.ts
@@ -1,5 +1,6 @@
 export * from './COCSOColorLogo';
 export * from './COCSOLogo';
+export * from './HMPLogo';
 export * from './HuonsLogo';
 export * from './KakaoLogo';
 export * from './NaverLogo';
--- packages/react-icons/src/components/semantic/index.ts
+++ packages/react-icons/src/components/semantic/index.ts
@@ -1,1 +1,7 @@
+export * from './ArrowIOSForwardIcon';
+export * from './CloseIcon';
+export * from './DeleteIcon';
+export * from './MenuIcon';
+export * from './OpenInNewIcon';
 export * from './SearchIcon';
+export * from './SideNavigationIcon';
