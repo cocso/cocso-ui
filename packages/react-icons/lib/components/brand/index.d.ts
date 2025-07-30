1ab03e646c759b480256bca678cd51ad225a6f94
ohprettyhak <me@haklee.me>
2025-07-30 15:22:24 +0900

    feat(icons): add KakaoOutlineLogo and NaverOutlineLogo components with type definitions

--- packages/react-icons/lib/components/brand/index.d.ts
+++ packages/react-icons/lib/components/brand/index.d.ts
@@ -2,5 +2,7 @@ export * from './COCSOColorLogo';
 export * from './COCSOLogo';
 export * from './HMPLogo';
 export * from './HuonsLogo';
+export * from './KakaoOutlineLogo';
 export * from './KakaoLogo';
+export * from './NaverOutlineLogo';
 export * from './NaverLogo';
