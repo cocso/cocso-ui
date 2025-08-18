7e29a570f45b44831b6597fa9b41657e448ccfb9
ohprettyhak <me@haklee.me>
2025-08-18 14:22:52 +0900

    refactor(dropdown): add styles and improve className handling for Dropdown component

--- packages/react/src/components/dropdown/Dropdown.tsx
+++ packages/react/src/components/dropdown/Dropdown.tsx
@@ -1,21 +1,22 @@
 import * as DropdownPrimitive from '@radix-ui/react-dropdown-menu';
+import { clsx as cn } from 'clsx';
 import { type ComponentPropsWithoutRef, type ComponentRef, forwardRef } from 'react';
-import { createClassName } from '../../utils/cn';
+import styles from './Dropdown.module.css';
 
 const DropdownContent = forwardRef<
   ComponentRef<typeof DropdownPrimitive.Content>,
   ComponentPropsWithoutRef<typeof DropdownPrimitive.Content>
 >(({ className, ...props }, ref) => {
-  const classNames = createClassName('cocso-dropdown-content', {}, [], className);
-  return <DropdownPrimitive.Content ref={ref} className={classNames} {...props} />;
+  return (
+    <DropdownPrimitive.Content ref={ref} className={cn(styles.content, className)} {...props} />
+  );
 });
 
 const DropdownItem = forwardRef<
   ComponentRef<typeof DropdownPrimitive.Item>,
   ComponentPropsWithoutRef<typeof DropdownPrimitive.Item>
 >(({ className, ...props }, ref) => {
-  const classNames = createClassName('cocso-dropdown-item', {}, [], className);
-  return <DropdownPrimitive.Item ref={ref} className={classNames} {...props} />;
+  return <DropdownPrimitive.Item ref={ref} className={cn(styles.item, className)} {...props} />;
 });
 
 export const Dropdown = Object.assign(DropdownPrimitive.Root, {
