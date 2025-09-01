5f32853bc710e6f3c1e4a9f397c445f01a20e6d8
ohprettyhak <me@haklee.me>
2025-09-01 16:19:56 +0900

    fix(package): update sideEffects and exports for CSS handling in package.json

--- packages/react/rollup.config.cjs
+++ packages/react/rollup.config.cjs
@@ -36,6 +36,7 @@ function buildJS(format, input, output) {
       postcss({
         modules: true,
         minimize: true,
+        extract: true
       }),
       resolve({ extensions }),
       isESM && commonjs(),
